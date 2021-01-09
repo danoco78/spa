d6a8f284a98d856d6d46f00f48467b544d328429
David <dnorena@celsia.com>
2021-01-08 20:49:42 -0500

    inicio de proyecto

---
+++ src/app/app.routes.ts
@@ -0,0 +1,34 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+// import { PageNotFoundComponent } from './';
+
+const APP_ROUTES: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'heroes', component: HeroesComponent },
+    // la siguiente ruta carga usando un parametro que vamos allamar id
+    { path: 'heroe/:id', component: HeroeComponent },
+    // { path: '**', component: PageNotFoundComponent },
+
+    // { path: 'path/:routeParam', component: MyComponent },
+    // { path: 'staticPath', component: ... },
+    // { path: '**', component: ... },
+    // { path: 'oldPath', redirectTo: '/staticPath' },
+    // { path: ..., component: ..., data: { message: 'Custom' }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(APP_ROUTES)],
+    exports: [RouterModule]
+})
+export class FeatureRoutingModule {}
+
+// se va a dejar sinel hsh para entnder en que partes es que falla por no tenerlo.
+// export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { useHash: true});
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
+
---
+++ src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import {ActivatedRoute } from '@angular/router';
+
+import { HeroesService } from '../../services/heroes.service';
+
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html'
+})
+export class HeroeComponent {
+
+  heroe: any = {};
+
+  constructor( private activateRoute: ActivatedRoute,
+               private _heroeService: HeroesService
+             ) {
+
+    this.activateRoute.params.subscribe( params => {
+      const idHeroe = 'id';
+      // console.log(params[idHeroe]);
+
+      this.heroe = this._heroeService.getHeroe(params[idHeroe]);
+      console.log(this.heroe);
+    });
+  }
+
+}
